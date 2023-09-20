1a7d89119b9aaccf5f69d52b94e285088f8811f5
guisantosalves <workguisantos@gmail.com>
2023-09-20 14:36:09 -0300

    starting project and making initial config

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+// pegar as informaçoes do context, se o user já estiver logado fazer mandar para o /dashboard
+export default function Login() {
+  return <div className={styles.mainContainer}>
+    <div className={styles.containerLogin}>
+      
+    </div>
+  </div>;
+}
